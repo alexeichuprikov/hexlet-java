b455c6bcbd6ba9e215a5ffb7c3f7999f17c3ac43
alexeichuprikov <alexei.chuprikov@gmail.com>
2025-09-29 14:29:05 +0300

    add commons-lang3-3.19.0.jar and add reverse One.java

--- io/hexlet/One.java
+++ io/hexlet/One.java
@@ -1,7 +1,11 @@
 package io.hexlet;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class One {
     public static void main(String[] args) {
-        Two.greeting();
+        var reverse = StringUtils.reverse("Hexlet");
+        System.out.println(reverse);
+        //Two.greeting();
     }
 }
